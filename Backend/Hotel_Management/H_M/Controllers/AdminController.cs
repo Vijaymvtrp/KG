6962c41579e55b4efeae488b68b9a3e79da7154b
srikanth narala <snarala@ruggedmonitoring.com>
2024-08-27 23:19:52 +0530

    Added Admin table, model and controller

---
+++ Backend/Hotel_Management/H_M/Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using H_M.Data;
+using H_M.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace H_M.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class AdminController : ControllerBase
+    {
+     private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmins()
+        {
+            var admins = await _context.Admin.ToListAsync();
+
+            if (admins == null || admins.Count == 0)
+            {
+                return Ok(new { message = "No Users/Admins are created upto now " });
+
+            }
+            else
+            {
+                return Ok(admins);
+            }
+            
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Admin>> AdminCreation(Admin admin)
+        {
+            if (admin == null)
+            {
+                return BadRequest(new { message = "Invalid admin data" });
+            }
+            try
+            {
+                _context.Admin.Add(admin);
+                await _context.SaveChangesAsync();
+                return  Ok(admin);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = " An occured while saving the Admin", error = ex.Message });
+            }
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> EditAdmin(int id, Admin admin)
+        {
+            if (id != admin.Id)
+            {
+                return BadRequest(new { message = $"Please create a admin/usr for {id}" });
+            }
+            try
+            {
+                _context.Admin.Update(admin);
+                await _context.SaveChangesAsync();
+
+
+                return Ok(new { message = $"Admin edit successfully for {id}" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ Backend/Hotel_Management/H_M/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace H_M.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public string type { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public Double MobileNumber { get; set; }
+        public string Company { get; set; }
+        public string Gst { get; set; }
+        public JsonElement Address { get; set; }
+    }
+}
